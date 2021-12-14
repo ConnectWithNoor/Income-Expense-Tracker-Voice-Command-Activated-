a936d3c0e2900963291bcf29b29c3a283a948a06
Noor Muhammad <connectwithnoor1@gmail.com>
2021-12-14 23:21:16 +0500

    charts adde

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+
 import { Doughnut } from 'react-chartjs-2';
+
+import useTransactions from '../../utils/useTransactions';
 
 import useStyles from './styles';
 
 function Details({ title }) {
+  const { total, chartData } = useTransactions(title);
   const classes = useStyles();
+
+  useLayoutEffect(() => {
+    ChartJS.register(ArcElement, Tooltip, Legend);
+  }, []);
 
   return (
     <Card className={title === 'Income' ? classes.income : classes.expense}>
       <CardHeader title={title} />
       <CardContent>
-        <Typography variant='h5'>$50</Typography>
-        {/* <Doughnut data='DATA' /> */}
+        <Typography variant='h5'>${total}</Typography>
+        <Doughnut data={chartData} />
       </CardContent>
     </Card>
   );
